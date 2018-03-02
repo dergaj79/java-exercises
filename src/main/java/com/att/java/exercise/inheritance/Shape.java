6a7eceb3ddaff13c7f28a1615ef4ccc041c1f76c
avi <dergaj79@gmail.com>
2018-03-02 14:33:40 +0200

    Shape and Circle Class done and tested with App class

--- src/main/java/com/att/java/exercise/inheritance/Shape.java
+++ src/main/java/com/att/java/exercise/inheritance/Shape.java
@@ -2,4 +2,40 @@ package com.att.java.exercise.inheritance;
 
 public class Shape {
 
+    protected String color;
+
+
+    //Constructors/
+    //Default Constructors
+    public Shape(){ }
+
+    //Constructor 2
+    public Shape(String color) {
+        this.color = color;
+    }
+
+    //Getters and Setters
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    //Methods
+    public double calcArea(double r, String color) {
+
+        throw new UnsupportedOperationException("Unknown Shape");
+
+    }
+    public double calcPerimeter(double r, String color){
+        throw new UnsupportedOperationException("Unknown Shape");
+    }
+
+
+    @Override
+    public String toString() {
+        return "Shape{" + "color='" + color + '\'' + '}';
+    }
 }
